498a48df195d85d8af1ecfd5b2795140cd04a5bc
shivamgupta02 <shivam.g@taltrip.com>
2024-07-30 12:20:24 +0530

    accordion

---
+++ src/components/accordion/data.js
@@ -0,0 +1,24 @@
+const data = [
+  {
+    id: "1",
+    question: "Question 1",
+    answer: "This is demo answer for question 1 ",
+  },
+  {
+    id: "2",
+    question: "Question 2",
+    answer: "This is demo answer for question 2 ",
+  },
+  {
+    id: "3",
+    question: "Question 3",
+    answer: "This is demo answer for question 3 ",
+  },
+  {
+    id: "4",
+    question: "Question 4",
+    answer: "This is demo answer for question 4 ",
+  },
+];
+
+export default data;
---
+++ src/components/accordion/index.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import data from "./data";
+import "./style.css";
+
+const Accordion = () => {
+  const [selected, setSelected] = useState(null);
+  function handleSingleSelection(getCurrentId) {
+    setSelected(getCurrentId === selected ? null : getCurrentId);
+  }
+
+  return (
+    <>
+      <div className="accordion">
+        <h1>Accordion</h1>
+        {data && data.length > 0
+          ? data.map((dataItem) => (
+              <div className="item">
+                <div
+                  className="title"
+                  onClick={() => handleSingleSelection(dataItem.id)}
+                  style={{
+                    cursor: "pointer",
+                  }}
+                >
+                  <h3>{dataItem.question}</h3>
+                  <span>+</span>
+                </div>
+                {selected === dataItem.id ? (
+                  <div className="content">{dataItem.answer}</div>
+                ) : null}
+              </div>
+            ))
+          : "No data available"}
+      </div>
+    </>
+  );
+};
+
+export default Accordion;
